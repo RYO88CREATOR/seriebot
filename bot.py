7c7fa1d4249a3c2ba0cf920a2bb15278355411b0
RYO88CREATOR <132718899+RYO88CREATOR@users.noreply.github.com>
2025-04-26 09:53:25 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,8 +10,9 @@ def ping():
     return "Bot attivo!"
 
 def run():
-    app.run(host="0.0.0.0", port=10000)
-    
+    import os
+port = int(os.environ.get("PORT", 10000))
+app.run(host="0.0.0.0", port=port)
 Thread(target=run).start()
 
 from telegram import (
