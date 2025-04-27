0f598c95b639ab33690141a040a9cdb5fb8f2ccb
RYO88CREATOR <132718899+RYO88CREATOR@users.noreply.github.com>
2025-04-28 00:14:48 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -158,7 +158,19 @@ SERIES_TV = {
             6: {"start_episode": 77, "num_episodes": 20},
         },
     },
-}
+    "Will & Grace": {
+    "channel_id": -1002499613359,
+    "seasons": {
+        1: {"start_episode": 2, "num_episodes": 22},
+        2: {"start_episode": 24, "num_episodes": 24},
+        3: {"start_episode": 48, "num_episodes": 25},
+        4: {"start_episode": 73, "num_episodes": 27},
+        5: {"start_episode": 100, "num_episodes": 24},
+        6: {"start_episode": 124, "num_episodes": 24},
+        7: {"start_episode": 148, "num_episodes": 24},
+        8: {"start_episode": 172, "num_episodes": 23},
+    },
+},
 
 ANIMATION = {
     "Castlevania": {
@@ -215,6 +227,11 @@ ANIMATION = {
             1: {"start_episode": 2, "num_episodes": 12},
             2: {"start_episode": 26, "num_episodes": 12},
         },
+    },
+    "Tekkaman": {
+        "channel_id": -1002697417639,
+        "seasons": {1: {"start_episode": 3,
+"num_episodes": 25},
     },
 }
 
